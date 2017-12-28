c6f935d13f033f95342cc1b788d6a80cffa8a429
antony-jr <antonyjr@protonmail.com>
2017-12-28 20:20:00 +0530

    fix install.sh!

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# 
+#!/bin/bash
 # BSD-3 Clause.
 # Copyright (C) 2017 Antony Jr.
 # 
@@ -11,22 +11,22 @@ repoRawUrl="https://raw.githubusercontent.com/antony-jr/QInstallerBridge/master/
 
 if [ $isCurl ]
 	then
-		echo "Installing " $packageName
-		echo "Creating directory:: " $packageName
+		echo Installing $packageName
+		echo Creating directory:: $packageName
 		mkdir $packageName
 		cd $packageName
-		echo "Downloading LICENSE and the latest files... "
-		curl -L "https://git.io/vbdTI" | bash # Install QArchive
-		curl -L "https://git.io/vbdTW" | bash # Install QEasyDownloader
-		curl -L $repoRawUrl$packageName".hpp" --output $packageName".hpp"
+		echo Downloading LICENSE and the latest files... 
+		curl -L https://git.io/vbdTI | bash # Install QArchive
+		curl -L https://git.io/vbdTW | bash # Install QEasyDownloader
+		curl -L $repoRawUrl$packageName.hpp --output $packageName.hpp
 		curl -L $repoRawUrl$license --output $license
-		echo "Installation complete!"
-		echo "Thank you for choosing "$packageName
+		echo Installation complete!
+		echo Thank you for choosing $packageName
 		cd ..
 		exit
 		
 	else
-		echo "Please install curl to continue the installation."
-		echo "Aborting... "
+		echo Please install curl to continue the installation.
+		echo Aborting... 
 		exit
 fi
